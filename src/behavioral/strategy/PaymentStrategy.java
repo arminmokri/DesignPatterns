436383c1b88513cf6ed989c15d4e83a09d051fa0
Armin Mokri <arminmokri@gmail.com>
2025-04-26 07:21:09 -0400

    add Strategy

--- src/behavioral/strategy/PaymentStrategy.java
+++ src/behavioral/strategy/PaymentStrategy.java
@@ -1,23 +1,5 @@
 package behavioral.strategy;
 
-//public abstract class PaymentStrategy {
-//
-//    protected Double amount;
-//    protected Boolean success;
-//
-//    Double getAmount() {
-//        return this.amount;
-//    }
-//
-//    Boolean isSucceed() {
-//        return this.success;
-//    }
-//
-//    abstract Boolean pay(Double amount);
-//
-//}
-
-
 public interface PaymentStrategy {
     Boolean pay(Double amount);
 }
